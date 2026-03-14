b03cc1a198a6e0588e22b75b5a7ce9b52d45be3f
Roeip2000 <roeicalendar34@gmail.com>
2026-03-14 21:10:21 +0200

    Revert "Re-apply navigation and CSS fixes after VS revert"
    This reverts commit 14523cff97fd2ae126bc95254df3f6f555051afe.

--- all.Master.designer.cs
+++ all.Master.designer.cs
@@ -31,7 +31,5 @@ namespace baker_ido
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-
-        protected global::System.Web.UI.WebControls.Button btnLogout;
     }
 }
